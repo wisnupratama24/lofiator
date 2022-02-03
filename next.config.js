2db217f90f1a23e621cf690bb6750f03ee3d6c36
Wisnu Pratama <wisnuputrapratama24@gmail.com>
2022-02-04 06:11:58 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withSass = require("@zeit/next-sass");
+
+module.exports = withSass({
+  cssModules: true,
+});
+
 module.exports = {
   reactStrictMode: true,
-}
+};
