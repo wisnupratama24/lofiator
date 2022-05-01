5ff4ec627a027401d5b15e2e16aa511011801211
wisnupratama24 <wisnuputrapratama24@gmail.com>
2022-05-01 13:49:23 +0700

    update: finish home page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import { LayoutProps } from "~/lib/types";
 function Layout({ children, ...props }: LayoutProps) {
   return (
     <main
-      className='lg:px-28 md:px-20 px-8 py-4 max-w-screen-xl mx-auto'
+      className='lg:px-28 md:px-20 px-8 max-w-screen-xl mx-auto overflow-x-hidden'
       {...props}>
       {children}
     </main>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -22,25 +22,27 @@ const navItems = [
 function Navbar() {
   return (
     <section>
-      <header className={styles.navbar}>
-        <Logo />
+      <div className='lg:px-28 md:px-20 px-8 max-w-screen-xl mx-auto fixed left-0 right-0 top-0 py-4 bg-white z-10'>
+        <header className={styles.navbar}>
+          <Logo />
 
-        <ul className='hidden lg:grid'>
-          {navItems.map((row, index) => {
-            return (
-              <li key={index}>
-                <Link href={row.link}>{row.name}</Link>
-              </li>
-            );
-          })}
-        </ul>
+          <ul className='hidden lg:grid'>
+            {navItems.map((row, index) => {
+              return (
+                <li key={index}>
+                  <Link href={row.link}>{row.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
 
-        <div>
-          <Link href='/register'>
-            <DefaultButton label='Daftar Sekarang' />
-          </Link>
-        </div>
-      </header>
+          <div>
+            <Link href='/register'>
+              <DefaultButton label='Daftar Sekarang' />
+            </Link>
+          </div>
+        </header>
+      </div>
     </section>
   );
 }
