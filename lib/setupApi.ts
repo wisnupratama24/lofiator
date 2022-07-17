d0115f6fe4e65a3d198a05198718a680b745f36d
Wisnu Pratama <wisnuputrapratama24>
2022-07-17 21:42:46 +0700

    update

--- lib/setupApi.ts
+++ lib/setupApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { getCookieUser } from "./helpers";
 export const BASE_URL = "http://lofiator-be.test";
+export const BASE_URL_PUBLIC = "https://8f0e-114-10-24-76.ap.ngrok.io";
+export const usePublic = false;
 export const setupApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: usePublic ? BASE_URL_PUBLIC : BASE_URL,
 });
 
 setupApi.interceptors.request.use(
